ae131e0404974b55bd97edc690de11008543b5b0
DanielFariias <danielfariias15@gmail.com>
2024-01-19 18:12:56 -0300

    Update database configuration

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const knexConfig: Knex.Config = {
-  client: 'sqlite',
+  client: env.DATABASE_CLIENT,
   connection: {
     filename: env.DATABASE_URL,
   },
